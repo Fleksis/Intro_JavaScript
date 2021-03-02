1f396a3a388c91173057c897e040c9fef139865d
Fleksis <markuss0303@gmail.com>
2021-03-02 14:14:22 +0200

    Pirmais JavaScript

---
+++ static/js/mans_skripts.js
@@ -0,0 +1,10 @@
+console.log('viss strādā!')
+//alert('Markuss') komentārs//
+alert('Hello!')
+
+let mainīgaisteksts = 'skola'
+
+let mainīgaisskaitlis = 17
+
+console.log(mainīgaisskaitlis)
+console.log(mainīgaisteksts)
